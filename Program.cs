f3b2136db7ea4e39ed8c77ce692897dca03c1bf5
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-07-19 17:19:43 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wk2Lab2___Temperature_Conversion_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
